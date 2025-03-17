8e49d2f7c1e74458d867db8df4d3576bc73e5a9f
Piotrekszmel <piotr.szmelczynski@gmail.com>
2025-03-17 14:59:06 +0100

    change 1

--- currency.py
+++ currency.py
@@ -1,5 +1,7 @@
 import requests
 from datetime import datetime, timedelta
+import csv
+
 
 currency = ["USD", "PLN"]
 base_url = "https://data.fixer.io/api/"
@@ -14,12 +16,22 @@ def get_currency(currency, date = "latest"):
     data = response.json()
     return data
 
-def load_currency_history(start_date: str, end_date: str):
+def load_currency_history(start_date: str, end_date: str, load_to_csv = True):
     start = datetime.strptime(start_date, "%Y-%m-%d")
     end = datetime.strptime(end_date, "%Y-%m-%d")
     delta = timedelta(days=1)
+    field_names = ['success', 'timestamp', 'historical', 'base', 'date', 'rates']
     while start <= end:
         date = start.strftime("%Y-%m-%d")
-        info = get_currency(currency, date)
+        info = get_currency(currency, date)     
         start += delta
-        return info
+        if load_to_csv == True:
+            with open("load_currency_history.csv", "w", newline="") as f:
+                w = csv.writer(f)
+                w.writerow(info.keys())
+                w.writerow(info.values())
+                f.close()
+                return "Data transfer complete"
+    
+
+print(load_currency_history('2013-04-03', '2013-04-06'))
